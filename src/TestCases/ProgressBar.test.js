50e78b4d9b61abbdf89bf7e30118e947e3bf823d
IndSaroj <kumascb@rbos.co.uk>
2023-02-13 20:46:00 +0530

    g

--- src/TestCases/ProgressBar.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import ProgressBar from '../ProgressBar/ProgressBar';
-
-
-test('Should include loan text', () => {
-  render(<ProgressBar />);
-  const linkElement = screen.getByText(/Loan/);
-  expect(linkElement).toBeInTheDocument();
-});
